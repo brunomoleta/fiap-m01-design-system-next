a1f81cb4fff5bf5f4ec785e4dc686f7a57b2dbd8
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-09-23 13:43:43 -0300

    :wrench: chore: adapt `tailwind.config.ts`

--- packages/design-system/tailwind.config.ts
+++ packages/design-system/tailwind.config.ts
@@ -3,9 +3,11 @@ import {spacing, colors, typography} from './src/tokens'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/docs/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
+    "./.storybook/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
