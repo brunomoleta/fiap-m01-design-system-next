eed82e02ece2074df8e55ee5426bef8eb6ae655c
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-09-27 14:56:23 -0300

    :recycle: refactor: `Divider` component

--- packages/design-system/src/components/Utils/Divider/component/Divider.tsx
+++ packages/design-system/src/components/Utils/Divider/component/Divider.tsx
@@ -1,15 +1,19 @@
-
-
 type Props = {
   /** Cor */
-  color?: 'light' | 'medium' | 'green';
+  color?: "background-accent" | "text-active" | "text-revert";
   /** O elemento é semântico? */
   isSemantic: boolean;
 };
 
 /** Serve para Separar componentes visualmente. */
-const Divider = ({ color = 'light', isSemantic }: Props) => {
-  return !isSemantic ? <div color={color} /> : <hr color={color} />;
+const Divider = ({ color = "text-revert", isSemantic }: Props) => {
+  const colorClass = `bg-${color}`;
+
+  return !isSemantic ? (
+    <div className={`h-px w-full ${colorClass}`} />
+  ) : (
+    <hr className={`h-px w-full border-0 ${colorClass}`} />
+  );
 };
 
 export default Divider;
