fb932a2448354147a24e6d5a434d958faf4262f1
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-09-27 14:59:50 -0300

    :recycle: refactor: `typography` token

---
+++ packages/design-system/src/tokens/typography/fontSize.ts
@@ -0,0 +1,8 @@
+export default {
+  "headline-xl": "2rem",
+  "headline-lg": "1.75rem",
+  "headline-base": "1.5rem",
+  "headline-sm": "1.25rem",
+  "text-base": "1.00rem",
+  "text-sm": "0.875rem",
+} as const;
---
+++ packages/design-system/src/tokens/typography/fontWeight.ts
@@ -0,0 +1,5 @@
+export default {
+  normal: 400,
+  medium: 600,
+  bold: 700,
+} as const;
