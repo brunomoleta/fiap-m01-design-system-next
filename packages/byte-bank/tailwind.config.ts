af947499d2155ff2844b2ec48181a8bf38ccc5db
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-09-28 14:04:13 -0300

    :tada: initial setup: byte-bank-app

---
+++ packages/byte-bank/tailwind.config.ts
@@ -0,0 +1,28 @@
+import type {Config} from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "background-accent": "hsl(9, 100%, 60%)",
+        "background-default": "hsl(192, 100%, 19%)",
+        "background-dark-grey": "hsl(183, 22%, 89%)",
+        "background-medium-grey": "hsl(0, 0%, 80%)",
+        "background-light-grey": "hsl(0, 0%, 97%)",
+        "background-light-green": "hsl(114, 22%, 91%)",
+        "background-fade": "hsla(191, 100%, 19%, 0.15)",
+        "text-revert": "hsl(330, 11%, 96%)",
+        "text-active": "hsl(111, 48%, 43%)",
+        "text-dark-green": "hsl(192, 100%, 19%)",
+        "text-default": "hsl(180, 10%, 15%)",
+      },
+    }
+  },
+  plugins: [],
+};
+export default config;
