37f8896015014764bae865b41786964f69225c05
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-10-03 17:35:43 -0300

    :recycle: refactor: `Button:hover`

--- packages/byte-bank/src/app/page.tsx
+++ packages/byte-bank/src/app/page.tsx
@@ -4,7 +4,7 @@ import {Header} from "$/app/components";
 export default function Home() {
   return (
     <>
-      <Header isLoggedIn={false}/>
+      <Header isLoggedIn={true}/>
       <div
         className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         bytebank
