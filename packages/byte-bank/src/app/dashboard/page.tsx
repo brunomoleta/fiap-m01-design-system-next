bb5dad8b5f93af8db63fde13a9314456075cd901
Gabriel <gabsato02@gmail.com>
2024-10-15 20:04:09 -0300

    Adiciona layout do componente de extrato

--- packages/byte-bank/src/app/dashboard/page.tsx
+++ packages/byte-bank/src/app/dashboard/page.tsx
@@ -96,7 +96,16 @@ export default function Home() {
           backgroundColor="background-light-grey"
           title="Extrato"
         >
-          teste
+          <div className="flex flex-col gap-spacing-md min-w-56 max-tablet:min-w-full">
+            <div className="flex items-center justify-between pb-spacing-sm border-b border-text-active">
+              <div className="flex flex-col gap-spacing-sm">
+                <span className="text-text-sm text-text-active">Novembro</span>
+                <span className="text-text-baseline">Depósito</span>
+                <span className="text-headline-sm font-bold">R$ 150</span>
+              </div>
+              <span className="text-text-sm text-text-grey">19/11/2022</span>
+            </div>
+          </div>
         </WidgetContainer>
       </main>
       <Footer/>
