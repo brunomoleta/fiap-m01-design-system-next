de9eb5d3a11d1fde28aefeb1fe150049087e9e76
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-10-26 11:34:04 -0300

    :recycle: refactor: export model schemas

--- packages/byte-bank/src/server/models/paymentMethods.model.ts
+++ packages/byte-bank/src/server/models/paymentMethods.model.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
-export interface ICard extends Document {
+export interface IPaymentMethod extends Document {
   cardNumber: string;
   cardName: string;
   cardType: 'debit' | 'credit' | 'debit and credit';
@@ -8,7 +8,7 @@ export interface ICard extends Document {
   cvv: string;
 }
 
-const CardSchema: Schema = new mongoose.Schema({
+export const PaymentMethodSchema: Schema = new mongoose.Schema({
   cardNumber: { type: String, required: true, match: [/^\d{16}$/, 'Card number must have 16 digits.'] },
   cardName: { type: String, required: true },
   cardType: { type: String, enum: ['debit', 'credit', 'debit and credit'], required: true },
@@ -16,6 +16,6 @@ const CardSchema: Schema = new mongoose.Schema({
   cvv: { type: String, required: true },
 });
 
-const CardModel: mongoose.Model<ICard> = mongoose.models.Card || mongoose.model("Card", CardSchema);
+const PaymentMethodsModel: mongoose.Model<IPaymentMethod> = mongoose.models.Card || mongoose.model("Card", PaymentMethodSchema);
 
-export default CardModel;
+export default PaymentMethodsModel;
