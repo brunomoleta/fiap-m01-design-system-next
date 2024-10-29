3d1b54bbd5414ba659db665bc3f929deb81f6374
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-10-29 11:02:09 -0300

    :recycle: refactor: insert `middleware` token

--- packages/byte-bank/src/server/services/transaction.service.ts
+++ packages/byte-bank/src/server/services/transaction.service.ts
@@ -1,6 +1,8 @@
 import { TransactionRepository } from '../repositories/transaction.repository';
 import { ITransaction } from '../models/transaction.model';
 import {IUser} from "$/server/models/user.model";
+import {verifyBearerToken} from "../middleware/token.middleware";
+import {NextRequest} from "next/server";
 
 export interface ICreateTransactionRequest {
   type: 'deposito' | 'transferencia' | 'saque' | 'compra' | 'pagamento';
@@ -15,23 +17,33 @@ export class TransactionService {
     this.repository = new TransactionRepository();
   }
 
-  async createTransaction(data:ICreateTransactionRequest) {
+  async createTransaction(data:ICreateTransactionRequest, req: NextRequest) {
+    verifyBearerToken(req)
+
     return await this.repository.create(data);
   }
 
-  async getAllTransactions(userId: string) {
+  async getAllTransactions(userId: string, req: NextRequest) {
+    verifyBearerToken(req)
+
     return await this.repository.getAll(userId);
   }
 
-  async getTransactionById(transactionId: string, userId: string) {
+  async getTransactionById(transactionId: string, userId: string, req: NextRequest) {
+    verifyBearerToken(req)
+
     return await this.repository.findOne(transactionId, userId);
   }
 
-  async updateTransaction(transactionId: string, data: Partial<ITransaction>) {
+  async updateTransaction(transactionId: string, data: Partial<ITransaction>,req: NextRequest) {
+    verifyBearerToken(req)
+
     return await this.repository.update(transactionId, data);
   }
 
-  async deleteTransaction(transactionId: string) {
+  async deleteTransaction(transactionId: string, req: NextRequest) {
+    verifyBearerToken(req)
+
     return await this.repository.delete(transactionId);
   }
 }
