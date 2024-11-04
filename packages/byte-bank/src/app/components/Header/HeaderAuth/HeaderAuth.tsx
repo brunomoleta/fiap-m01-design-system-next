c1c3c6d97ff90e167ac4d984663b1eaa5a1d0645
Luiz Moraes <luizotcunha@hotmail.com>
2024-11-04 17:38:44 -0300

    Inserindo caminho para editar ususario

--- packages/byte-bank/src/app/components/Header/HeaderAuth/HeaderAuth.tsx
+++ packages/byte-bank/src/app/components/Header/HeaderAuth/HeaderAuth.tsx
@@ -1,25 +1,31 @@
-import {UserIcon} from "../../../../../../design-system/src";
+import { UserIcon } from "../../../../../../design-system/src";
+import { useRouter } from 'next/navigation';
 import NavigationMenu from "$/app/components/Header/HeaderAuth/NavigationMenu";
 
 interface Props {
   username: string;
 }
 
-const HeaderAuth = ({username}: Props) => {
+const HeaderAuth = ({ username }: Props) => {
+  const router = useRouter();
+
+  const handleUserMenuClick = () => {
+    router.push('/dashboard/edituser');
+  };
+
   return (
     <>
-      <div
-        className='max-tablet:justify-between max-w-page-width-desktop flex gap-7 tablet:justify-end items-center w-full'>
-        <NavigationMenu/>
-        <span className={'text-text-revert max-tablet:hidden'}>
+      <div className='max-tablet:justify-between max-w-page-width-desktop flex gap-7 tablet:justify-end items-center w-full'>
+        <NavigationMenu />
+        <span className='text-text-revert max-tablet:hidden'>
           {username}
         </span>
-        <div aria-label='menu do usuário'>
-          <UserIcon height={40} width={40} fill="background-accent"/>
+        <div aria-label='menu do usuário' onClick={handleUserMenuClick} style={{ cursor: 'pointer' }}>
+          <UserIcon height={40} width={40} fill="background-accent" />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default HeaderAuth;
